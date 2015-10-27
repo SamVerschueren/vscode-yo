eb0092070ee7402f6efabb99bb7cbdb112bb75b0
Sam Verschueren <sam.verschueren@gmail.com>
2015-10-27 17:27:55 +0100

    Added checkbox prompt

---
+++ src/prompts/checkbox.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+import {window, QuickPickItem, QuickPickOptions} from 'vscode';
+import Prompt from './prompt';
+
+const figures = require('figures');
+
+export default class CheckboxPrompt extends Prompt {
+
+	constructor(question: any) {
+		super(question);
+	}
+
+	public render() {
+		let choices = this._question.choices.reduce((result, choice) => {
+			result[`${choice.checked === true ? figures.circleFilled : figures.circle} ${choice.name}`] = choice;
+			return result;
+		}, {});
+
+		const options: QuickPickOptions = {
+			placeHolder: `${this._question.message} (Press Esc to continue)`
+		};
+
+		return window.showQuickPick(Object.keys(choices), options)
+			.then(result => {
+				if (result !== undefined) {
+					choices[result].checked = !choices[result].checked;
+
+					return this.render();
+				}
+
+				return this._question.choices.reduce((result, choice) => {
+					if (choice.checked === true) {
+						result.push(choice.value);
+					}
+
+					return result;
+				}, []);
+			});
+	}
+}
--- src/prompts/factory.ts
+++ src/prompts/factory.ts
@@ -4,6 +4,7 @@ import Prompt from './prompt';
 import InputPrompt from './input';
 import ListPrompt from './list';
 import ConfirmPrompt from './confirm';
+import CheckboxPrompt from './checkbox';
 
 export default class PromptFactory {
 	public static createPrompt(question: any): Prompt {
@@ -22,6 +23,8 @@ export default class PromptFactory {
 				return new ListPrompt(question);
 			case 'confirm':
 				return new ConfirmPrompt(question);
+			case 'checkbox':
+				return new CheckboxPrompt(question);
 			default:
 				throw new Error(`Could not find a prompt for question type ${question.type}`);
 		}
