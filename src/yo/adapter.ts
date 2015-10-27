2321cfac45e9ee6891942b53e2ad31e5a9397b5e
Sam Verschueren <sam.verschueren@gmail.com>
2015-10-27 07:38:32 +0100

    Run generator after all input is processed

--- src/yo/adapter.ts
+++ src/yo/adapter.ts
@@ -6,26 +6,31 @@ import PromptFactory from '../prompts/factory';
 export default class CodeAdapter {
 
 	public prompt(questions, callback) {
+		let answers = {};
+
 		var promise = questions.reduce((promise, question) => {
 			return promise.then(() => {
 				return PromptFactory.createPrompt(question);
 			}).then(prompt => {
 				return prompt.render();
 			}).then(result => {
-				console.log(result);
+				answers[question.name] = result;
 			});
 		}, Promise.resolve());
 
-		promise.catch(err => {
-			window.showErrorMessage(err.message);
-		});
+		promise
+			.then(() => {
+				console.log(JSON.stringify(answers, undefined, '  '));
+				callback(answers);
+			})
+			.catch(err => {
+				window.showErrorMessage(err.message);
+			});
 	}
 
 	public diff(actual, expected) {
 		console.log(actual);
 	}
 
-	public log() {
-		console.log(arguments);
-	}
+	public log = require('yeoman-environment/lib/util/log')();
 }
